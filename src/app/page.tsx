5c70254fe117f2e584db84fe3eb52f7f08862207
Hércules Costa <dev.hrcules@gmail.com>
2025-04-24 15:47:56 -0300

    KNE-208 style: finalizando a UI com os elementos no Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { Button } from "@heroui/react";
 import { playfairDisplay } from "./layout";
 import { ExternalLink, Instagram, Linkedin } from "lucide-react";
 
+import decorLines from "../../public/images/decorLines.png";
+import decorTraits from "../../public/images/decorTraits.png";
 import ImageHero from "../../public/images/Fundo.png";
 
 import styles from "@/styles/page.module.css";
@@ -21,19 +23,44 @@ export default function Home() {
           className={styles.appHeroImage}
         />
         <h1 className={`${styles.appHeroTitle} ${playfairDisplay.className}`}>
-          Somos uma agência de <br /> marketing digital <br /> com DNA{" "}
-          <span>100%</span> digital.
+          <div className={styles.heroDecorWrapper}>
+            <Image
+              src={decorTraits}
+              alt="Traço decorativo"
+              className={styles.heroDecorImg}
+            />
+            <span className={styles.heroDecorText}>Somos uma agência de</span>
+          </div>
+          <br />
+          <div className={styles.heroUnderlineWrapper}>
+            <span className={styles.heroUnderlineText}>MARKETING DIGITAL</span>
+            <Image
+              src={decorLines}
+              alt="Traço decorativo"
+              className={styles.heroUnderlineImg}
+            />
+          </div>
+          <br />
+          com DNA <span className={styles.heroHighlight}>100%</span>{" "}
+          <span className={styles.heroDecoration}>
+            DIGITAL.
+            <Image
+              src={decorLines}
+              alt="Traço decorativo"
+              className={styles.heroLineBelow}
+            />
+          </span>
         </h1>
 
         <div className={styles.appHeroButtons}>
           <Button isIconOnly radius="sm">
-            <Instagram size={20} />
+            <Instagram size={24} />
           </Button>
-          <Button radius="sm" endContent={<ExternalLink size={16} />}>
+          <Button radius="sm" endContent={<ExternalLink size={18} />}>
             Conheça mais
           </Button>
           <Button isIconOnly radius="sm">
-            <Linkedin size={20} />
+            <Linkedin size={24} />
           </Button>
         </div>
       </div>
