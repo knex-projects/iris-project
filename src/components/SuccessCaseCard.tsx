4b05440bd24e74b678734e2b9d9dcb959bf35828
Hércules Costa <dev.hrcules@gmail.com>
2025-08-05 16:12:50 -0300

    chore(portfolio): atualiza o conteúdo do portfólio da empresa

--- src/components/SuccessCaseCard.tsx
+++ src/components/SuccessCaseCard.tsx
@@ -3,22 +3,32 @@ import { StaticImageData } from "next/image";
 
 import styles from "../styles/components/SuccessCaseCard.module.css";
 
+export interface SucessCaseProps {
+  logo: StaticImageData;
+  name: string;
+  description: string;
+}
 interface SuccessCaseCardProps {
   logo: StaticImageData;
   name: string;
   isActive?: boolean;
+  onClick: () => void;
 }
 
 const SuccessCaseCard = ({
   logo,
   name,
   isActive = false,
+  onClick,
 }: SuccessCaseCardProps) => {
+  console.log(isActive);
+
   return (
     <div
-      className={`${styles.SuccessCaseCardContainer} ${
+      className={`${styles.SuccessCaseCardContainer} border-2 ${
         isActive ? "border-green-500" : "border-transparent"
       }`}
+      onClick={onClick}
     >
       <Image src={logo} alt={`Logo da ${name}`} />
     </div>
